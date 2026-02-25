323d1b025361d656ea759e521cdcfbf39986f1f7
Aidar <renys_northside@mail.ru>
2026-02-25 09:49:45 +0500

    added the minio config

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+        config: {
+            provider: "aws-s3",
+            providerOptions: {
+                accessKeyId: env("MINIO_ACCESS_KEY"),
+                secretAccessKey: env("MINIO_SECRET_KEY"),
+                endpoint: env("MINIO_ENDPOINT"),
+                s3ForcePathStyle: true,
+                signatureVersion: "v4",
+                params: {
+                    Bucket: env("MINIO_BUCKET"),
+                },
+            },
+        },
+    },
+});
