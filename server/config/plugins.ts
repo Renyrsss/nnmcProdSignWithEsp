76d181fa52d2efce15102e0461f573d3c16869f6
Aidar <renys_northside@mail.ru>
2026-02-25 10:34:56 +0500

    v5 strapi aws s3

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -3,11 +3,13 @@ module.exports = ({ env }) => ({
         config: {
             provider: "aws-s3",
             providerOptions: {
-                accessKeyId: env("MINIO_ACCESS_KEY"),
-                secretAccessKey: env("MINIO_SECRET_KEY"),
+                credentials: {
+                    accessKeyId: env("MINIO_ACCESS_KEY"),
+                    secretAccessKey: env("MINIO_SECRET_KEY"),
+                },
+                region: "us-east-1",
                 endpoint: env("MINIO_ENDPOINT"),
-                s3ForcePathStyle: true,
-                signatureVersion: "v4",
+                forcePathStyle: true,
                 params: {
                     Bucket: env("MINIO_BUCKET"),
                 },
